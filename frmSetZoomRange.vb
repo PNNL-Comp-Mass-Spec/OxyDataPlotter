ed271eed0a9d69a287bb4b70b475f7fe14038269
alchemistmatt <matthew.monroe@pnnl.gov>
2017-03-30 18:32:32 -0700

    Auto-generated code regenerated

--- frmSetZoomRange.vb
+++ frmSetZoomRange.vb
@@ -48,65 +48,62 @@ Friend Class frmSetZoomRange
 	'It can be modified using the Windows Form Designer.
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container
+        Me.components = New System.ComponentModel.Container()
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
-        Me.txtYStart = New System.Windows.Forms.TextBox
-        Me.txtYEnd = New System.Windows.Forms.TextBox
-        Me.txtXEnd = New System.Windows.Forms.TextBox
-        Me.cmdCancel = New System.Windows.Forms.Button
-        Me.cmdSet = New System.Windows.Forms.Button
-        Me.txtXStart = New System.Windows.Forms.TextBox
-        Me.lblYStart = New System.Windows.Forms.Label
-        Me.lblYEnd = New System.Windows.Forms.Label
-        Me.lblXEnd = New System.Windows.Forms.Label
-        Me.lblXStart = New System.Windows.Forms.Label
+        Me.txtYStart = New System.Windows.Forms.TextBox()
+        Me.txtYEnd = New System.Windows.Forms.TextBox()
+        Me.txtXEnd = New System.Windows.Forms.TextBox()
+        Me.cmdCancel = New System.Windows.Forms.Button()
+        Me.cmdSet = New System.Windows.Forms.Button()
+        Me.txtXStart = New System.Windows.Forms.TextBox()
+        Me.lblYStart = New System.Windows.Forms.Label()
+        Me.lblYEnd = New System.Windows.Forms.Label()
+        Me.lblXEnd = New System.Windows.Forms.Label()
+        Me.lblXStart = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'txtYStart
         '
         Me.txtYStart.AcceptsReturn = True
-        Me.txtYStart.AutoSize = False
         Me.txtYStart.BackColor = System.Drawing.SystemColors.Window
         Me.txtYStart.Cursor = System.Windows.Forms.Cursors.IBeam
         Me.txtYStart.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtYStart.ForeColor = System.Drawing.SystemColors.WindowText
-        Me.txtYStart.Location = New System.Drawing.Point(104, 72)
+        Me.txtYStart.Location = New System.Drawing.Point(125, 89)
         Me.txtYStart.MaxLength = 0
         Me.txtYStart.Name = "txtYStart"
         Me.txtYStart.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.txtYStart.Size = New System.Drawing.Size(89, 19)
+        Me.txtYStart.Size = New System.Drawing.Size(107, 23)
         Me.txtYStart.TabIndex = 5
         Me.txtYStart.Text = "0"
         '
         'txtYEnd
         '
         Me.txtYEnd.AcceptsReturn = True
-        Me.txtYEnd.AutoSize = False
         Me.txtYEnd.BackColor = System.Drawing.SystemColors.Window
         Me.txtYEnd.Cursor = System.Windows.Forms.Cursors.IBeam
         Me.txtYEnd.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtYEnd.ForeColor = System.Drawing.SystemColors.WindowText
-        Me.txtYEnd.Location = New System.Drawing.Point(104, 96)
+        Me.txtYEnd.Location = New System.Drawing.Point(125, 118)
         Me.txtYEnd.MaxLength = 0
         Me.txtYEnd.Name = "txtYEnd"
         Me.txtYEnd.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.txtYEnd.Size = New System.Drawing.Size(89, 19)
+        Me.txtYEnd.Size = New System.Drawing.Size(107, 24)
         Me.txtYEnd.TabIndex = 7
         Me.txtYEnd.Text = "10"
         '
         'txtXEnd
         '
         Me.txtXEnd.AcceptsReturn = True
-        Me.txtXEnd.AutoSize = False
         Me.txtXEnd.BackColor = System.Drawing.SystemColors.Window
         Me.txtXEnd.Cursor = System.Windows.Forms.Cursors.IBeam
         Me.txtXEnd.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtXEnd.ForeColor = System.Drawing.SystemColors.WindowText
-        Me.txtXEnd.Location = New System.Drawing.Point(104, 40)
+        Me.txtXEnd.Location = New System.Drawing.Point(125, 49)
         Me.txtXEnd.MaxLength = 0
         Me.txtXEnd.Name = "txtXEnd"
         Me.txtXEnd.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.txtXEnd.Size = New System.Drawing.Size(89, 19)
+        Me.txtXEnd.Size = New System.Drawing.Size(107, 24)
         Me.txtXEnd.TabIndex = 3
         Me.txtXEnd.Text = "10"
         '
@@ -117,13 +114,14 @@ Friend Class frmSetZoomRange
         Me.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.cmdCancel.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cmdCancel.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.cmdCancel.Location = New System.Drawing.Point(208, 48)
+        Me.cmdCancel.Location = New System.Drawing.Point(250, 59)
         Me.cmdCancel.Name = "cmdCancel"
         Me.cmdCancel.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.cmdCancel.Size = New System.Drawing.Size(73, 25)
+        Me.cmdCancel.Size = New System.Drawing.Size(87, 31)
         Me.cmdCancel.TabIndex = 9
         Me.cmdCancel.Tag = "4020"
         Me.cmdCancel.Text = "&Cancel"
+        Me.cmdCancel.UseVisualStyleBackColor = False
         '
         'cmdSet
         '
@@ -131,27 +129,27 @@ Friend Class frmSetZoomRange
         Me.cmdSet.Cursor = System.Windows.Forms.Cursors.Default
         Me.cmdSet.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cmdSet.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.cmdSet.Location = New System.Drawing.Point(208, 16)
+        Me.cmdSet.Location = New System.Drawing.Point(250, 20)
         Me.cmdSet.Name = "cmdSet"
         Me.cmdSet.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.cmdSet.Size = New System.Drawing.Size(73, 25)
+        Me.cmdSet.Size = New System.Drawing.Size(87, 30)
         Me.cmdSet.TabIndex = 8
         Me.cmdSet.Tag = "14510"
         Me.cmdSet.Text = "&Set"
+        Me.cmdSet.UseVisualStyleBackColor = False
         '
         'txtXStart
         '
         Me.txtXStart.AcceptsReturn = True
-        Me.txtXStart.AutoSize = False
         Me.txtXStart.BackColor = System.Drawing.SystemColors.Window
         Me.txtXStart.Cursor = System.Windows.Forms.Cursors.IBeam
         Me.txtXStart.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtXStart.ForeColor = System.Drawing.SystemColors.WindowText
-        Me.txtXStart.Location = New System.Drawing.Point(104, 16)
+        Me.txtXStart.Location = New System.Drawing.Point(125, 20)
         Me.txtXStart.MaxLength = 0
         Me.txtXStart.Name = "txtXStart"
         Me.txtXStart.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.txtXStart.Size = New System.Drawing.Size(89, 19)
+        Me.txtXStart.Size = New System.Drawing.Size(107, 23)
         Me.txtXStart.TabIndex = 1
         Me.txtXStart.Text = "0"
         '
@@ -161,10 +159,10 @@ Friend Class frmSetZoomRange
         Me.lblYStart.Cursor = System.Windows.Forms.Cursors.Default
         Me.lblYStart.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblYStart.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.lblYStart.Location = New System.Drawing.Point(8, 72)
+        Me.lblYStart.Location = New System.Drawing.Point(10, 89)
         Me.lblYStart.Name = "lblYStart"
         Me.lblYStart.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.lblYStart.Size = New System.Drawing.Size(93, 21)
+        Me.lblYStart.Size = New System.Drawing.Size(111, 25)
         Me.lblYStart.TabIndex = 4
         Me.lblYStart.Text = "Y Start"
         '
@@ -174,10 +172,10 @@ Friend Class frmSetZoomRange
         Me.lblYEnd.Cursor = System.Windows.Forms.Cursors.Default
         Me.lblYEnd.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblYEnd.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.lblYEnd.Location = New System.Drawing.Point(8, 96)
+        Me.lblYEnd.Location = New System.Drawing.Point(10, 118)
         Me.lblYEnd.Name = "lblYEnd"
         Me.lblYEnd.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.lblYEnd.Size = New System.Drawing.Size(89, 21)
+        Me.lblYEnd.Size = New System.Drawing.Size(106, 26)
         Me.lblYEnd.TabIndex = 6
         Me.lblYEnd.Text = "Y End"
         '
@@ -187,10 +185,10 @@ Friend Class frmSetZoomRange
         Me.lblXEnd.Cursor = System.Windows.Forms.Cursors.Default
         Me.lblXEnd.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblXEnd.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.lblXEnd.Location = New System.Drawing.Point(8, 40)
+        Me.lblXEnd.Location = New System.Drawing.Point(10, 49)
         Me.lblXEnd.Name = "lblXEnd"
         Me.lblXEnd.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.lblXEnd.Size = New System.Drawing.Size(89, 21)
+        Me.lblXEnd.Size = New System.Drawing.Size(106, 26)
         Me.lblXEnd.TabIndex = 2
         Me.lblXEnd.Text = "X End"
         '
@@ -200,20 +198,20 @@ Friend Class frmSetZoomRange
         Me.lblXStart.Cursor = System.Windows.Forms.Cursors.Default
         Me.lblXStart.Font = New System.Drawing.Font("Arial", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblXStart.ForeColor = System.Drawing.SystemColors.ControlText
-        Me.lblXStart.Location = New System.Drawing.Point(8, 16)
+        Me.lblXStart.Location = New System.Drawing.Point(10, 20)
         Me.lblXStart.Name = "lblXStart"
         Me.lblXStart.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.lblXStart.Size = New System.Drawing.Size(93, 21)
+        Me.lblXStart.Size = New System.Drawing.Size(111, 26)
         Me.lblXStart.TabIndex = 0
         Me.lblXStart.Text = "X Start"
         '
         'frmSetZoomRange
         '
         Me.AcceptButton = Me.cmdSet
-        Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+        Me.AutoScaleBaseSize = New System.Drawing.Size(6, 16)
         Me.BackColor = System.Drawing.SystemColors.Control
         Me.CancelButton = Me.cmdCancel
-        Me.ClientSize = New System.Drawing.Size(293, 124)
+        Me.ClientSize = New System.Drawing.Size(404, 201)
         Me.ControlBox = False
         Me.Controls.Add(Me.txtYStart)
         Me.Controls.Add(Me.txtYEnd)
@@ -239,9 +237,9 @@ Friend Class frmSetZoomRange
         Me.ResumeLayout(False)
 
     End Sub
-#End Region 
+#End Region
 
-	Public Sub ReturnValues(ByRef dblXStart As Double, ByRef dblXEnd As Double, ByRef dblYStart As Double, ByRef dblYEnd As Double)
+    Public Sub ReturnValues(ByRef dblXStart As Double, ByRef dblXEnd As Double, ByRef dblYStart As Double, ByRef dblYEnd As Double)
         dblXStart = SharedVBNetRoutines.VBNetRoutines.CDblSafe(txtXStart.Text)
         dblXEnd = SharedVBNetRoutines.VBNetRoutines.CDblSafe(txtXEnd.Text)
 		
