918ed011bdd1e53025f93c5373a36138cc81b6db
alchemistmatt <matthew.monroe@pnnl.gov>
2017-03-31 23:51:55 -0700

    Update the version

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -1,6 +1,4 @@
 Imports System.Reflection
-Imports System.Runtime.CompilerServices
-Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
@@ -29,6 +27,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below
 
 
-<Assembly: AssemblyVersion("2.42.*")> 
+<Assembly: AssemblyVersion("3.0.*")>
 
 
